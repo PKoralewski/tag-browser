39b4150019cfe0ddb6f25b989e1c010b47633e83
PioKor01 <piotr.koralewski@emkaem.pl>
2024-04-01 20:17:07 +0200

    style: add no data styles

--- src/components/NoData.tsx
+++ src/components/NoData.tsx
@@ -3,7 +3,7 @@ import { Flex, Text } from "@chakra-ui/layout"
 const NoData = () => {
 	return (
 		<Flex w='100%' justifyContent={"center"}>
-			<Text fontSize={40} color={"gray.600"}>
+			<Text fontSize={[24, 32, 36, 40]} color={"gray.600"}>
 				No data
 			</Text>
 		</Flex>
