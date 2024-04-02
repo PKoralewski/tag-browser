6602577d366a74605b6f9b3f4351dc657c5bf93e
PioKor01 <piotr.koralewski@emkaem.pl>
2024-04-02 13:38:20 +0200

    style: add props param

--- src/components/NoData.tsx
+++ src/components/NoData.tsx
@@ -1,9 +1,10 @@
 import { Flex, Text } from "@chakra-ui/layout"
+import { TextProps } from "@chakra-ui/react"
 
-const NoData = () => {
+const NoData = ({ ...props }: TextProps) => {
 	return (
 		<Flex w='100%' justifyContent={"center"}>
-			<Text fontSize={[24, 32, 36, 40]} color={"gray.600"}>
+			<Text fontSize={[24, 32, 36, 40]} color={"gray.600"} {...props}>
 				No data
 			</Text>
 		</Flex>
