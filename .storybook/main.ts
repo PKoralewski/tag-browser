70bb88897dd29e91096511e4910030d0bedb87a4
PioKor01 <piotr.koralewski@emkaem.pl>
2024-04-02 14:57:32 +0200

    feat: loader story

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,11 +4,11 @@ const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     "@storybook/preset-create-react-app",
-    "@storybook/addon-onboarding",
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@chromatic-com/storybook",
     "@storybook/addon-interactions",
+    '@chakra-ui/storybook-addon'
   ],
   framework: {
     name: "@storybook/react-webpack5",
--- src/stories/Pagination.stories.tsx
+++
@@ -1,8 +0,0 @@
-import Pagination from "../components/Pagination"
-
-export default {
-	title: "Pagination",
-	component: Pagination,
-}
-
-export const Default = () => <Pagination page={1} totalPages={25} onPageChange={() => {}} />
