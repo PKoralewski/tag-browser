670f3c9aa184133c6f70f8674b0e09afc8337e1d
PioKor01 <piotr.koralewski@emkaem.pl>
2024-04-02 13:53:43 +0200

    style: add props param

--- src/components/SortSelectOrder.tsx
+++ src/components/SortSelectOrder.tsx
@@ -1,10 +1,11 @@
 import { Flex, Text } from "@chakra-ui/layout"
 import { useState } from "react"
+import { SelectProps } from "@chakra-ui/react"
 
 import SelectField from "./SelectField"
 import { ISortSelectOrderProps } from "../types/SortSelectOrder"
 
-const SortSelectOrder = ({ onOrderChange }: ISortSelectOrderProps) => {
+const SortSelectOrder = ({ onOrderChange, ...props }: ISortSelectOrderProps & SelectProps) => {
 	const [ordersArray] = useState([
 		{
 			name: "Descending",
@@ -26,6 +27,7 @@ const SortSelectOrder = ({ onOrderChange }: ISortSelectOrderProps) => {
 				bgColor={"#FFF"}
 				h={["30px", "35px"]}
 				color='#333'
+				{...props}
 				values={ordersArray}
 				onSelectChange={onOrderChange}
 			/>
